b0bd208398a95f5798fa1b8131b1d8f37d11644e
Kautiontape <kautiontape@gmail.com>
2015-07-13 13:35:06 -0400

    Added script

---
+++ doomsday.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import random
+import time
+import re
+
+days = ['su', 'm', 'tu', 'w', 'th', 'f', 'sa']
+
+def randomDate(start, end, prop):
+    stime = time.mktime(time.strptime(start, "%m/%d/%Y"))
+    etime = time.mktime(time.strptime(end, "%m/%d/%Y"))
+
+    ptime = stime + prop * (etime - stime)
+    return time.localtime(ptime)
+
+def getDayIndex(day):
+    for i in enumerate(days):
+        try:
+            if re.match(i[1], day):
+                return int(i[0])
+        except TypeError:
+            pass
+
+def printHint(date, hint):
+    year = int(time.strftime("%Y", date))
+    c = year // 100
+    y = year % 100
+
+    anchor = (5 * (c % 4) + days.index('tu')) % 7
+    a = y // 12
+    b = y % 12
+    c = b // 4
+    d = a + b + c
+    doomsday = (d + anchor) % 7
+
+    print("Anchor day is: ", days[anchor])
+    if hint > 0:
+        print("Doomsday is: ", days[doomsday])
+
+def promptGuess(dd, hints = 0):
+    ddStr = time.strftime('%m/%d/%Y', dd)
+    expected = int(time.strftime("%w", dd))
+
+    result = { 'result' : False, 'hints' : hints }
+
+    guess = input("What day of the week is %s: " % ddStr).lower()
+    match = re.match("(" + "|".join(days) + "|[0-6]|h)", guess)
+    if match:
+        if re.match('h', guess):
+            printHint(dd, hints)
+            result['hints'] = result['hints'] + 1
+            return result
+
+        guess = getDayIndex(guess)
+        if expected != guess:
+            print("Sorry. That is not correct")
+            return result
+
+        print("Correct!")
+        result['result'] = True
+        return result
+    else:
+        print("Not a valid guess. Please enter a day of the week (or 'h' for Hint)")
+        return result
+
+    return result
+
+def playGame():
+    dd = randomDate("1/1/1700", "12/31/2500", random.random())
+
+    finished = False
+    hints = 0
+    while(not finished):
+        result = promptGuess(dd, hints)
+        finished = result['result']
+        hints = result['hints']
+
+def main():
+    while(True):
+        playGame()
+        a = input("\nPlay another game [Y/n]? ")
+        if re.match("n", a):
+            return False
+
+
+if __name__ == "__main__":
+    main()
